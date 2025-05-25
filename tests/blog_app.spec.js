02ad1fa1a02e387b5ef071163119d7e92a033a95
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2025-05-25 13:27:49 +0300

    osa5 - tehtävä 5.18 tehty

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -19,4 +19,28 @@ describe('Blog app', () => {
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByText('login')).toBeVisible()
   })
+
+  describe('Login', () => {
+    // Test login fail before testing login success
+    // After login success the user is logged in
+    test('fails with wrong credentials', async ({ page }) => {
+        await page.goto('http://localhost:5173')
+
+        await page.getByRole('textbox').first().fill('wrongusername')
+        await page.getByRole('textbox').last().fill('wrongpassword')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('wrong username or password')).toBeVisible()
+    })
+
+    test('succeeds with correct credentials', async ({ page }) => {
+        await page.goto('http://localhost:5173')
+
+        await page.getByRole('textbox').first().fill('magicadehex')
+        await page.getByRole('textbox').last().fill('magiaa123')
+        await page.getByRole('button', { name: 'login' }).click()
+        
+        await expect(page.getByText('Welcome, Milla Magia!')).toBeVisible()
+    })
+  })
 })

e19365ec69cf7ee42786b225a48144844a138a20
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2025-05-26 16:06:18 +0300

    osa5 - tehtävä 5.21 tehty

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+import { loginWith, createBlogWith } from './helper'
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -24,9 +25,11 @@ describe('Blog app', () => {
     test('fails with wrong credentials', async ({ page }) => {
         await page.goto('http://localhost:5173')
 
-        await page.getByTestId('username').fill('wrongusername')
-        await page.getByTestId('password').fill('wrongpassword')
-        await page.getByRole('button', { name: 'login' }).click()
+        await loginWith(page, 'wrongusername', 'wrongpassword')
+
+        //await page.getByTestId('username').fill('wrongusername')
+        //await page.getByTestId('password').fill('wrongpassword')
+        //await page.getByRole('button', { name: 'login' }).click()
 
         await expect(page.getByText('wrong username or password')).toBeVisible()
     })
@@ -34,9 +37,11 @@ describe('Blog app', () => {
     test('succeeds with correct credentials', async ({ page }) => {
         await page.goto('http://localhost:5173')
 
-        await page.getByTestId('username').fill('magicadehex')
-        await page.getByTestId('password').fill('magiaa123')
-        await page.getByRole('button', { name: 'login' }).click()
+        await loginWith(page, 'magicadehex', 'magiaa123')
+
+        //await page.getByTestId('username').fill('magicadehex')
+        //await page.getByTestId('password').fill('magiaa123')
+        //await page.getByRole('button', { name: 'login' }).click()
         
         await expect(page.getByText('Welcome, Milla Magia!')).toBeVisible()
     })
@@ -46,42 +51,65 @@ describe('Blog app', () => {
     beforeEach(async ({ page }) => {
       await page.goto('http://localhost:5173')
 
-      await page.getByTestId('username').fill('magicadehex')
-      await page.getByTestId('password').fill('magiaa123')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'magicadehex', 'magiaa123')
+
+      //await page.getByTestId('username').fill('magicadehex')
+      //await page.getByTestId('password').fill('magiaa123')
+      //await page.getByRole('button', { name: 'login' }).click()
     })
   
     test('a new blog can be created', async ({ page }) => {
       await expect(page.getByText('create new blog')).toBeVisible()
-      await page.getByRole('button', { name: 'create new blog' }).click()
+      //await page.getByRole('button', { name: 'create new blog' }).click()
 
-      await page.getByTestId('title').fill('all about magic potions')
-      await page.getByTestId('author').fill('Milla Magia')
-      await page.getByTestId('url').fill('magicpotions.com')
+      await createBlogWith(page, 'all about magic potions', 'Milla Magia', 'magicpotions.com')
 
-      await page.getByRole('button', { name: 'create' }).click()
+      //await page.getByTestId('title').fill('all about magic potions')
+      //await page.getByTestId('author').fill('Milla Magia')
+      //await page.getByTestId('url').fill('magicpotions.com')
+
+      //await page.getByRole('button', { name: 'create' }).click()
       await expect(page.getByText('all about magic potions by Milla Magia', { exact: true })).toBeVisible()
     })
 
-    describe('and when there is a blog', () => {
+    describe('and when there are a couple of blogs', () => {
       beforeEach(async ({ page }) => {
         // Create a blog post
-        await page.getByRole('button', { name: 'create new blog' }).click()
-        await page.getByTestId('title').fill('all about magic potions')
-        await page.getByTestId('author').fill('Milla Magia')
-        await page.getByTestId('url').fill('magicpotions.com')
-        await page.getByRole('button', { name: 'create' }).click()
+        await createBlogWith(page, 'all about magic potions', 'Milla Magia', 'magicpotions.com')
+        await createBlogWith(page, 'life at Vesuvius mountain', 'Milla Magia', 'vesuviuslife.com')
+        //await page.getByRole('button', { name: 'create new blog' }).click()
+        //await page.getByTestId('title').fill('all about magic potions')
+        //await page.getByTestId('author').fill('Milla Magia')
+        //await page.getByTestId('url').fill('magicpotions.com')
+        //await page.getByRole('button', { name: 'create' }).click()
       })
 
-      test('that blog can be liked', async ({ page }) => {
-        await expect(page.getByText('show more information')).toBeVisible()
-        await page.getByRole('button', { name: 'show more information' }).click()
+      test('a blog can be liked', async ({ page }) => {
+        await expect(page.getByText('show more information').first()).toBeVisible()
+        await page.getByRole('button', { name: 'show more information' }).first().click()
   
-        await expect(page.getByText('like this blog')).toBeVisible()
-        await expect(page.getByText('Likes: 0')).toBeVisible()
-        await page.getByRole('button', { name: 'like this blog' }).click()
+        await expect(page.getByText('like this blog').first()).toBeVisible()
+        await expect(page.getByText('Likes: 0').first()).toBeVisible()
+        await page.getByRole('button', { name: 'like this blog' }).first().click()
   
-        await expect(page.getByText('Likes: 1')).toBeVisible()      
+        await expect(page.getByText('Likes: 1').first()).toBeVisible()      
+      })
+
+      test('a blog can be deleted', async ({ page }) => {
+        await expect(page.getByText('all about magic potions by Milla Magia', { exact: true })).toBeVisible()
+        await expect(page.getByText('life at Vesuvius mountain by Milla Magia', { exact: true })).toBeVisible()
+
+        await expect(page.getByText('delete this blog').first()).toBeVisible()
+
+        page.on('dialog', async (dialog) => {
+          expect(dialog.message()).toContain('Delete the blog')
+          await dialog.accept()
+        })
+
+        await page.getByRole('button', { name: 'delete this blog' }).first().click()
+
+        await expect(page.getByText('all about magic potions by Milla Magia', { exact: true })).not.toBeVisible()
+        await expect(page.getByText('life at Vesuvius mountain by Milla Magia', { exact: true })).toBeVisible()
       })
     })
   })
